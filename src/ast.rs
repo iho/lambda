dad0412e606c0a65a1efab36d18755edf8421787
iho <iho@users.noreply.github.com>
2024-06-19 15:28:41 +0300

    I have no idea what I am doing

--- src/ast.rs
+++ src/ast.rs
@@ -1,5 +1,7 @@
+#[derive(Debug, Clone, PartialEq, Eq)]
 pub enum Expr {
     Var(String),
     Abs(String, Box<Expr>),
     App(Box<Expr>, Box<Expr>),
+    Alias(String, Box<Expr>),
 }
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,64 @@
+use crate::ast::Expr;
+use std::{collections::HashMap, env, rc::Rc};
+
 use lalrpop_util::lalrpop_mod;
 
 lalrpop_mod!(pub grammar);
 pub mod ast;
 fn main() {
-    grammar::ExprParser::new().parse("\\ x .  x").unwrap();
-    grammar::ExprParser::new().parse("\\ x . \\ y . x").unwrap();
-    grammar::ExprParser::new().parse("\\ x . \\ y . y").unwrap();
-    println!("Hello world!") 
+    let mut env: HashMap<String, Expr> = HashMap::new();
+    let mut contex: HashMap<String, Expr> = HashMap::new();
+    let mut variable_counter = 1;
+
+
+    let k = grammar::ExprParser::new()
+        .parse("let k = \\ x . \\y . x")
+        .unwrap();
+    let s = grammar::ExprParser::new()
+        .parse("let s = \\x.\\y.\\z.((x_z)_(y_z))")
+        .unwrap();
+    let skk = grammar::ExprParser::new().parse("((s_k)_k)").unwrap();
+
+    eval(k, &mut env, &mut contex,  &mut variable_counter);
+    eval(s, &mut env ,&mut contex, &mut variable_counter);
+    println!("{:?}", env);
+    let res = eval(skk.clone(), &mut env,&mut contex, &mut variable_counter);
+    let res = eval(res.clone(), &mut env,&mut contex, &mut variable_counter);
+
+    println!("{:?}", env);
+    println!("{:?}", res);
+}
+
+fn eval(expr:Expr, env: &mut HashMap<String, Expr>, contex: &mut HashMap<String, Expr>,mut variable_counter: &mut i32) -> Expr {
+    match expr {
+        Expr::Var(s) => {
+            if let Some(e) = env.get(&s) {
+               return  eval(e.clone(), env, contex, variable_counter);
+            } 
+            
+            Expr::Var(s)
+        }
+        Expr::Alias(name, body) => {
+            env.insert(name.clone(), *body.clone());
+            Expr::Var(name)
+        }
+        Expr::Abs(e1, e2) => {
+            for (k, v) in contex.iter() {
+                if *v == *e2 {
+                    return eval(v.clone(), env, contex, variable_counter) ;
+                }
+            }
+            let new_var = format!("var_{}", variable_counter);
+            *variable_counter += 1;
+            contex.insert(new_var.clone(), Expr::Var(e1.clone()));
+            let res =  eval(*e2.clone(), env, contex, variable_counter);
+            Expr::Abs(new_var, Box::new(res))
+        }
+        Expr::App(s, e) => {
+            let res_s =  eval(*s.clone(), env, contex, variable_counter);
+            let res_e =  eval(*e.clone(), env, contex, variable_counter);
+
+            Expr::App(Box::new(res_s), Box::new(res_e))
+        }
+    } 
 }
