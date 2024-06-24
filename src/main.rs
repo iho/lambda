2bad1f519af1ab440e0b19b6418a2094387ea53c
iho <iho@users.noreply.github.com>
2024-06-24 15:50:52 +0300

    final

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use crate::ast::Expr;
-use std::{collections::hash_set::HashSet, collections::HashMap};
+use std::{collections::HashMap, collections::HashSet};
 
 use lalrpop_util::lalrpop_mod;
 
@@ -8,63 +8,50 @@ pub mod ast;
 
 fn main() {
     let mut env: HashSet<String> = HashSet::new();
-    let mut contex: HashMap<String, ast::Expr> = HashMap::new();
+    let mut context: HashMap<String, Expr> = HashMap::new();
     let mut variable_counter = 1;
 
     let k = grammar::ExprParser::new()
-        .parse("let k = \\ x . \\y . x")
+        .parse("let k = \\x. \\y. x")
         .unwrap();
     let s = grammar::ExprParser::new()
-        .parse("let s = \\x.\\y.\\z.((x_z)_(y_z))")
+        .parse("let s = \\x. \\y. \\z. ((x_z)_(y_z))")
         .unwrap();
     let skk = grammar::ExprParser::new().parse("((s_k)_k)").unwrap();
 
-    eval(k, &mut env, &mut contex, &mut variable_counter);
-    eval(s, &mut env, &mut contex, &mut variable_counter);
-    let res = eval(skk.clone(), &mut env, &mut contex, &mut variable_counter);
-    println!("{:?}", env);
+    eval(k, &mut env, &mut context, &mut variable_counter);
+    eval(s, &mut env, &mut context, &mut variable_counter);
+    let res = eval(skk.clone(), &mut env, &mut context, &mut variable_counter);
     println!("{:?}", res);
-    let res = normalize(res.clone(), &mut env, &mut contex, &mut variable_counter);
-    // println!("{:?}", env);
+    let res = normalize(res.clone(), &mut env, &mut context, &mut variable_counter);
     println!("{:?}", res);
 }
+
 fn eval(
     expr: Expr,
     env: &mut HashSet<String>,
     context: &mut HashMap<String, Expr>,
-    mut variable_counter: &mut i32,
+    variable_counter: &mut i32,
 ) -> Expr {
     match expr {
         Expr::Var(s) => {
-            env.insert(s.clone());
             if let Some(e) = context.get(&s) {
-                return eval(e.clone(), env, context, variable_counter);
+                eval(e.clone(), env, context, variable_counter)
+            } else {
+                Expr::Var(s)
             }
-
-            Expr::Var(s)
         }
         Expr::Alias(name, body) => {
             context.insert(name.clone(), *body.clone());
-            env.insert(name.clone());
             Expr::Var(name)
         }
         Expr::Lam(e1, e2) => {
-            for (k, v) in context.iter() {
-                if *v == *e2 {
-                    return eval(v.clone(), env, context, variable_counter);
-                }
-            }
-            let new_var = format!("var_{}", variable_counter);
-            *variable_counter += 1;
-            context.insert(new_var.clone(), Expr::Var(e1.clone()));
-            env.insert(new_var.clone());
             let res = eval(*e2.clone(), env, context, variable_counter);
-            Expr::Lam(new_var, Box::new(res))
+            Expr::Lam(e1, Box::new(res))
         }
         Expr::App(s, e) => {
             let res_s = eval(*s.clone(), env, context, variable_counter);
             let res_e = eval(*e.clone(), env, context, variable_counter);
-
             Expr::App(Box::new(res_s), Box::new(res_e))
         }
     }
@@ -76,53 +63,34 @@ fn substitute(
     value: Expr,
     env: &mut HashSet<String>,
     context: &mut HashMap<String, Expr>,
-    mut variable_counter: &mut i32,
+    variable_counter: &mut i32,
 ) -> Expr {
     match expr {
         Expr::Var(s) => {
-            env.insert(s.clone());
             if s == var {
-                return value;
+                value
+            } else {
+                Expr::Var(s)
             }
-            Expr::Var(s)
         }
         Expr::Lam(e1, e2) => {
             if e1 == var {
-                println!("Variable {} is bound", e1);
-                // variable is bound, do not substitute
-                Expr::Lam(
-                    e1,
-                    Box::new(substitute(*e2, var, value, env, context, variable_counter)),
-                )
-            } else if env.contains(&e1) {
-                println!("Variable {} is already in the environment", e1);
+                // Variable is bound, do not substitute
+                Expr::Lam(e1, e2)
+            } else {
                 let new_var = format!("var_{}", variable_counter);
                 *variable_counter += 1;
-                context.insert(new_var.clone(), Expr::Var(e1.clone()));
-                env.insert(new_var.clone());
-                let t1_prime = substitute(
-                    ast::Expr::Var(new_var.clone()),
-                    e1,
-                    *e2.clone(),
+                let body = substitute(
+                    *e2,
+                    e1.clone(),
+                    Expr::Var(new_var.clone()),
                     env,
                     context,
                     variable_counter,
                 );
                 Expr::Lam(
-                    new_var,
-                    Box::new(substitute(
-                        *e2,
-                        var,
-                        t1_prime,
-                        env,
-                        context,
-                        variable_counter,
-                    )),
-                )
-            } else {
-                Expr::Lam(
-                    e1,
-                    Box::new(substitute(*e2, var, value, env, context, variable_counter)),
+                    new_var.clone(),
+                    Box::new(substitute(body, var, value, env, context, variable_counter)),
                 )
             }
         }
@@ -155,7 +123,7 @@ fn beta_reduction(
     expr: Expr,
     env: &mut HashSet<String>,
     context: &mut HashMap<String, Expr>,
-    mut variable_counter: &mut i32,
+    variable_counter: &mut i32,
 ) -> Expr {
     match expr {
         Expr::App(e1, e2) => {
@@ -169,28 +137,23 @@ fn beta_reduction(
             let res2 = beta_reduction(*e2, env, context, variable_counter);
             Expr::App(Box::new(res1), Box::new(res2))
         }
-        Expr::Lam(var, body) => {
-            env.remove(&var);
-            Expr::Lam(
-                var,
-                Box::new(beta_reduction(*body, env, context, variable_counter)),
-            )
-        }
-        Expr::Var(s) => {
-            env.insert(s.clone());
-            Expr::Var(s)
-        }
+        Expr::Lam(var, body) => Expr::Lam(
+            var,
+            Box::new(beta_reduction(*body, env, context, variable_counter)),
+        ),
+        Expr::Var(s) => Expr::Var(s),
         Expr::Alias(name, body) => Expr::Alias(
             name,
             Box::new(beta_reduction(*body, env, context, variable_counter)),
         ),
     }
 }
+
 fn normalize(
     expr: Expr,
     env: &mut HashSet<String>,
     context: &mut HashMap<String, Expr>,
-    mut variable_counter: &mut i32,
+    variable_counter: &mut i32,
 ) -> Expr {
     let mut res = expr.clone();
     loop {
